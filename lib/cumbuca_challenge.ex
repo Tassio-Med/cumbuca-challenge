28b2eb015622db2e0730c71d345ea92f3b177ccc
Tassio-Med <tassiohfmed@gmail.como>
2024-07-18 16:57:44 -0300

    feat: implement collect_monarchs function

--- lib/cumbuca_challenge.ex
+++ lib/cumbuca_challenge.ex
@@ -1,5 +1,13 @@
 defmodule CumbucaChallenge do
   def execute do
     IO.puts("Adicione os nomes dos reis e rainhas de Cumbúquia, cada um por linha e um de casa vez. Após adicionar o nome pressione Enter. Quando quiser finalizar, apenas pressione Enter em uma linha vazia.")
+    monarchs = collect_monarchs()
+  end
+
+  defp collect_monarchs(acc \\ []) do
+    case IO.gets("") do
+      "\n" -> Enum.reverse(acc)
+      monarch -> collect_monarchs([String.trim(monarch) | acc])
+    end
   end
 end
