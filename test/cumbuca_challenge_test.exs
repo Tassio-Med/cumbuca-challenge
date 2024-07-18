7b7a021a7b48c59fb61bf2861653df4e06bd02dd
Tassio-Med <tassiohfmed@gmail.como>
2024-07-18 17:13:25 -0300

    test: implemented test for the added number_monarchs/1 function

--- test/cumbuca_challenge_test.exs
+++ test/cumbuca_challenge_test.exs
@@ -1,8 +1,20 @@
 defmodule CumbucaChallengeTest do
   use ExUnit.Case
-  doctest CumbucaChallenge
 
-  test "greets the world" do
-    assert CumbucaChallenge.hello() == :world
+  alias CumbucaChallenge
+
+  test "A função number_monarchs/1 numera os nomes de forma correta" do
+    names = ["Filipe", "Francisco", "Filipe", "Francisco", "Filipe"]
+    expected = ["Filipe I", "Francisco I", "Filipe II", "Francisco II", "Filipe III"]
+    assert CumbucaChallenge.number_monarchs(names) == expected
+
+    names = ["Wallyson", "Célia", "Cecília", "Wallyson"]
+    expected = ["Wallyson I", "Célia I", "Cecília I", "Wallyson II"]
+    assert CumbucaChallenge.number_monarchs(names) == expected
+
+    names = ["Miguel", "Miguel", "Miguel", "Miguel"]
+    expected = ["Miguel I", "Miguel II", "Miguel III", "Miguel IV"]
+    assert CumbucaChallenge.number_monarchs(names) == expected
+
   end
 end
