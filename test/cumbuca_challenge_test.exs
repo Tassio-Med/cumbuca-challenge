c6d24085030dc1ca5e64ffc572aa78a96ac45cc7
Tassio-Med <tassiohfmed@gmail.como>
2024-07-18 17:14:51 -0300

    test: implemented test for the to_roman/1 function

--- test/cumbuca_challenge_test.exs
+++ test/cumbuca_challenge_test.exs
@@ -15,6 +15,20 @@ defmodule CumbucaChallengeTest do
     names = ["Miguel", "Miguel", "Miguel", "Miguel"]
     expected = ["Miguel I", "Miguel II", "Miguel III", "Miguel IV"]
     assert CumbucaChallenge.number_monarchs(names) == expected
+  end
 
+  test "A função to_roman/1 converte os números para algarismos romanos corretamente " do
+    assert CumbucaChallenge.to_roman(1) == "I"
+    assert CumbucaChallenge.to_roman(3) == "III"
+    assert CumbucaChallenge.to_roman(4) == "IV"
+    assert CumbucaChallenge.to_roman(9) == "IX"
+    assert CumbucaChallenge.to_roman(18) == "XVIII"
+    assert CumbucaChallenge.to_roman(40) == "XL"
+    assert CumbucaChallenge.to_roman(50) == "L"
+    assert CumbucaChallenge.to_roman(55) == "LV"
+    assert CumbucaChallenge.to_roman(93) == "XCIII"
+    assert CumbucaChallenge.to_roman(100) == "C"
+    assert CumbucaChallenge.to_roman(500) == "D"
+    assert CumbucaChallenge.to_roman(1000) == "M"
   end
 end
