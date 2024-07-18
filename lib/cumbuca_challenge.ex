05e220a9dab5bfa7d9edb1d9bacb2e70b658c3f4
Tassio-Med <tassiohfmed@gmail.como>
2024-07-18 17:10:47 -0300

    feat: line break added to input

--- lib/cumbuca_challenge.ex
+++ lib/cumbuca_challenge.ex
@@ -1,6 +1,6 @@
 defmodule CumbucaChallenge do
   def execute do
-    IO.puts("Adicione os nomes dos reis e rainhas de Cumbúquia, cada um por linha e um de casa vez. Após adicionar o nome pressione Enter. Quando quiser finalizar, apenas pressione Enter em uma linha vazia.")
+    IO.puts("Adicione os nomes dos reis e rainhas de Cumbúquia, cada um por linha e um de casa vez. \nApós adicionar o nome pressione Enter. \nQuando quiser finalizar, apenas pressione Enter em uma linha vazia. \nInsira os nomes abaixo:")
     monarchs = collect_monarchs()
     numbered_monarchs = number_monarchs(monarchs)
     Enum.each(numbered_monarchs, &IO.puts/1)
